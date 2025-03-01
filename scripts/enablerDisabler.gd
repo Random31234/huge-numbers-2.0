1db987fa11b77e72ff97c5fbce0c01dd99f8fd1f
Random31234 <randorte3@gmail.com>
2025-03-01 12:53:35 -0800

    Improvement to ui and patch bugs

--- scripts/enablerDisabler.gd
+++ scripts/enablerDisabler.gd
@@ -6,30 +6,20 @@ extends CheckButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var toggled_on = self.toggle_mode
 	
-	if toggled_on == false:
-		for n in nonLegacy:
-			n.process_mode =Node.PROCESS_MODE_INHERIT
-			n.visible = true
-		for n in legacy:
-			n.process_mode = Node.PROCESS_MODE_DISABLED
-			n.visible = false
-	if toggled_on == true:
-		for n in nonLegacy:
-			n.process_mode = Node.PROCESS_MODE_DISABLED
-			n.visible = false
-		for n in legacy:
-			n.process_mode = Node.PROCESS_MODE_INHERIT
-			n.visible = true
+	Toggle(self.button_pressed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	await(self.toggled)
+	Toggle(self.button_pressed)
 
 
-func Toggle(toggled_on: bool) -> void:
+func Toggle(toggled_o: bool) -> void:
+	print(toggled_o)
+	var toggled_on
+	toggled_on = self.button_pressed
 	if toggled_on == false:
 		for n in nonLegacy:
 			n.process_mode =Node.PROCESS_MODE_INHERIT
