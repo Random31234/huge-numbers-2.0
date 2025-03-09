4db43d0a64cc63449b230f18b8091af846dfc7f9
Random31234 <randorte3@gmail.com>
2025-03-08 19:52:35 -0800

    Update maths

--- scripts/math.gd
+++ scripts/math.gd
@@ -3,6 +3,7 @@ class_name math
 @export var varNames:Array[String]
 @export var varVal:Array[Big]
 @export var calculation:String
+@export var name:String
 # Called when the node enters the scene tree for the first time.
 func _ready(names:Array[String], val:Array[Big], calculations:String) -> void:
 	varNames = names
--- scripts/maths.gd
+++ scripts/maths.gd
@@ -3,12 +3,25 @@ extends Node
 @export var m:math
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var vale =math.new()
-	var b:Array[Big]
-	b.append(Big.new(1,2))
-	b.append(Big.new(1,3))
-	vale.varVal = b
-	maths.append(vale)
+	#test function
+	#var vale =math.new()
+	#var b:Array[Big]
+	#b.append(Big.new(1,2))
+	#b.append(Big.new(1,3))
+	#vale.varVal = b
+	#maths.append(vale)
+	pass
+
+
+func searchForMath(s:String)->math:
+	
+	for x in maths:
+		
+		if (x.name == s):
+			return x
+	
+	return m
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
