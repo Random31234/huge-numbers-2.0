cc471713b764481bbac07524377f55abd0da0417
Random31234 <randorte3@gmail.com>
2025-03-08 22:40:12 -0800

    Updates for maths, connecting stats

--- scripts/maths.gd
+++ scripts/maths.gd
@@ -1,6 +1,7 @@
 extends Node
 @export var maths:Array[math]
 @export var m:math
+@export var stat:stats
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#test function
@@ -22,7 +23,17 @@ func searchForMath(s:String)->math:
 	
 	return m
 
+func performCalculation(ma:math):
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func searchStringInArray(a:Array,s:String):
+	
+	for x in a:
+		if x == s:
+			return true
+	
+	return false
