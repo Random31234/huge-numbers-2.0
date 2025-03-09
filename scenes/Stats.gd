01b4a02b7aecffa10938052f97baa9617ff41847
Random31234 <randorte3@gmail.com>
2025-03-08 22:54:33 -0800

    Update stats script and storage

--- scenes/Stats.gd
+++ scenes/Stats.gd
@@ -1,6 +1,9 @@
 extends Node
 class_name stats
 
+@export var names:Array[String]
+@export var values:Array[Big]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
