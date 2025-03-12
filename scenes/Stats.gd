b21f834acc62a411d7f253e90c6c6d816d88644f
Random31234 <randorte3@gmail.com>
2025-03-11 23:19:38 -0700

    Update the stats

--- scenes/Stats.gd
+++ scenes/Stats.gd
@@ -3,7 +3,10 @@ class_name stats
 
 @export var names:Array[String]
 @export var values:Array[Big]
+@export var statBox:VBoxContainer
 
+var statEntity = preload("res://scenes/StatEntity.tscn")
+var swappable:bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +20,9 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func swappableT(b:bool):
+	swappable = b
+	updateStats()
 
 #this function swaps the values.
 func swapper(id1:int,id2:int):
@@ -30,3 +36,29 @@ func swapper(id1:int,id2:int):
 	names[id2] = name
 	values[id1] = values[id2]
 	values[id2] =v
+
+
+#this function is to be called for updates.
+func updateStats():
+	
+	for n in statBox.get_children():
+		statBox.remove_child(n)
+		n.queue_free()
+		
+	var z
+	var i
+	z =0
+	for xyz in values:
+		i = statEntity.instantiate()
+		i.get_child(0).text = names[z] + " : " +values[z].toScientific()
+		
+		
+		if(swappable == true):
+			i.get_child(1).visibility = true
+			i.get_child
+		if(swappable == false):
+			i.get_child(1).visibility = false
+			i.get_child(1)
+			
+		z+=1
+		
